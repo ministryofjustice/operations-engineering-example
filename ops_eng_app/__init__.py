2c643301be145d0c0d28833594f3ba09dba8b4c2
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-11-17 08:32:54 +0000

    use 5000 port again

--- ops_eng_app/__init__.py
+++ ops_eng_app/__init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_gudeatama():
-    return "<p>Hello Gudetama! Better eaten than rotten! Seriously I just can't. Learn to restm not to quit.</p>"
+def hello_gudetama():
+    return "<p>Hello Gudetama! Better eaten than rotten! Seriously I just can't. Learn to rest not to quit.</p>"
